2f7b2db492858c878fe399423eeb7b637c503d82
zhangmaokai <zmkfml@163.com>
2023-11-06 19:20:48 +0800

    feat: 首页-热门推荐

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,7 +2,7 @@
  * @Author: zhangmaokai zmkfml@163.com
  * @Date: 2023-11-03 00:09:36
  * @LastEditors: zhangmaokai zmkfml@163.com
- * @LastEditTime: 2023-11-06 18:42:20
+ * @LastEditTime: 2023-11-06 19:10:51
  * @FilePath: /erabbit-uni-app-vue3-ts/src/pages/index/index.vue
  * @Description: 首页
 -->
@@ -10,13 +10,15 @@
   <CustomNavbar></CustomNavbar>
   <XtxSwiper :list="bannerList"></XtxSwiper>
   <CategoryPanel :list="cateoryList"></CategoryPanel>
+  <HotPanel :list="hotList"></HotPanel>
 </template>
 
 <script setup lang="ts">
 import CustomNavbar from './components/CustomNavbar.vue'
 import CategoryPanel from './components/CategoryPanel.vue'
-import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home'
-import type { BannerItem, CategoryItem } from '@/types/home'
+import HotPanel from './components/HotPanel.vue'
+import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/home'
+import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 
@@ -34,10 +36,18 @@ const getHomeCategoryData = async () => {
   cateoryList.value = res.result
 }
 
+// 获取首页热门推荐
+const hotList = ref<HotItem[]>([])
+const getHomeHotData = async () => {
+  const res = await getHomeHotAPI()
+  hotList.value = res.result
+}
+
 // 小程序生命周期
 onLoad(() => {
   getHomeBannerData()
   getHomeCategoryData()
+  getHomeHotData()
 })
 </script>
 
--- src/types/home.d.ts
+++ src/types/home.d.ts
@@ -2,7 +2,7 @@
  * @Author: zhangmaokai zmkfml@163.com
  * @Date: 2023-11-06 16:33:44
  * @LastEditors: zhangmaokai zmkfml@163.com
- * @LastEditTime: 2023-11-06 18:36:56
+ * @LastEditTime: 2023-11-06 19:09:43
  * @FilePath: /erabbit-uni-app-vue3-ts/src/types/home.d.ts
  * @Description: 首页-数据类型
  */
@@ -27,4 +27,20 @@ export type CategoryItem = {
   id: string
   /** 分类名称 */
   name: string
+}
+
+/** 首页-热门推荐数据类型 */
+export type HotItem = {
+  /** 说明 */
+  alt: string
+  /** id */
+  id: string
+  /** 图片集合[ 图片路径 ] */
+  pictures: string[]
+  /** 跳转地址 */
+  target: string
+  /** 标题 */
+  title: string
+  /** 推荐类型 */
+  type: string
 }
