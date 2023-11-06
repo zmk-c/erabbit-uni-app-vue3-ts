499161598c12175a1e252cd63f9a9dbec14d3db9
zhangmaokai <zmkfml@163.com>
2023-11-06 15:49:54 +0800

    feat: 首页-CustomNavbar导航+自适应

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,26 +2,18 @@
  * @Author: zhangmaokai zmkfml@163.com
  * @Date: 2023-11-03 00:09:36
  * @LastEditors: zhangmaokai zmkfml@163.com
- * @LastEditTime: 2023-11-06 09:51:28
+ * @LastEditTime: 2023-11-06 15:27:26
  * @FilePath: /erabbit-uni-app-vue3-ts/src/pages/index/index.vue
  * @Description: 首页
 -->
-<script setup lang="ts">
-//
-</script>
-
 <template>
-  <uni-card
-    title="基础卡片"
-    sub-title="副标题"
-    extra="额外信息"
-    thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
-  >
-    <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
-  </uni-card>
-
+  <CustomNavbar></CustomNavbar>
   <view class="index">这是首页</view>
 </template>
+
+<script setup lang="ts">
+import CustomNavbar from './components/CustomNavbar.vue'
+</script>
 
 <style lang="scss">
 //
