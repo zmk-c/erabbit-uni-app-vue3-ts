a5732f95ed33a3c60b16a6cabf15c34af6e9d6bc
zhangmaokai <zmkfml@163.com>
2023-11-06 10:53:04 +0800

    feat: uni-ui配置

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,7 +2,7 @@
  * @Author: zhangmaokai zmkfml@163.com
  * @Date: 2023-11-03 00:09:36
  * @LastEditors: zhangmaokai zmkfml@163.com
- * @LastEditTime: 2023-11-03 09:58:54
+ * @LastEditTime: 2023-11-06 09:51:28
  * @FilePath: /erabbit-uni-app-vue3-ts/src/pages/index/index.vue
  * @Description: 首页
 -->
@@ -11,6 +11,15 @@
 </script>
 
 <template>
+  <uni-card
+    title="基础卡片"
+    sub-title="副标题"
+    extra="额外信息"
+    thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+  >
+    <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
+  </uni-card>
+
   <view class="index">这是首页</view>
 </template>
 
