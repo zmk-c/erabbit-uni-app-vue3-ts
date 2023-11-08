b4b758b8f476f50a6830cc794056547af5ac22d6
zhangmaokai <zmkfml@163.com>
2023-11-08 14:04:41 +0800

    feat: 轮播图交互

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -4,15 +4,15 @@
     <view class="goods">
       <!-- 商品主图 -->
       <view class="preview">
-        <swiper circular>
+        <swiper circular @change="onChange">
           <swiper-item v-for="item in goods?.mainPictures" :key="item">
-            <image mode="aspectFill" :src="item" />
+            <image mode="aspectFill" :src="item" @tap="onTapImage(item)" />
           </swiper-item>
         </swiper>
         <view class="indicator">
-          <text class="current">1</text>
+          <text class="current">{{ currentIndex + 1 }}</text>
           <text class="split">/</text>
-          <text class="total">5</text>
+          <text class="total">{{ goods?.mainPictures.length }}</text>
         </view>
       </view>
 
@@ -132,6 +132,21 @@ const getGoodsByIdData = async () => {
 onLoad(() => {
   getGoodsByIdData()
 })
+
+// 轮播图点击事件
+const currentIndex = ref(0)
+const onChange: UniHelper.SwiperOnChange = (ev) => {
+  currentIndex.value = ev.detail!.current
+}
+
+// 点击图片对应的事件
+const onTapImage = (imgUrl: string) => {
+  // 大图预览
+  uni.previewImage({
+    current: imgUrl,
+    urls: goods.value!.mainPictures, // 需要预览的图片链接列表 必传参数
+  })
+}
 </script>
 
 <style lang="scss">
